35588a973dd73d97cd990843f2924b00ef352c7d
Petro Davydov <arturshopengauergik@gmail.com>
2025-04-09 21:18:55 +0300

    add to skelet

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Birthday(Field):
         try:
             self.value = datetime.strptime(value, "%d.%m.%Y").date()
         except ValueError:
-            raise ValueError("Invalid date format. Use DD.MM.YYYY") + Fore.RED
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
         super().__init__(self.value)
 
     def __str__(self):
